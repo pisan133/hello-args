4e98049e37c674e4818a59ecd0c08f3a431dafd9
Yusuf Pisan <pisan@uw.edu>
2020-01-03 12:01:32 -0800

    2020 changes

--- simplecompile.sh
+++ simplecompile.sh
@@ -13,17 +13,11 @@ date
 echo "*** Compiling C program"
 gcc -std=c11 -Wall -Wextra -Wno-sign-compare *.c -g -o myprogram.exe
 
-echo "*** cpplint"
-cpplint *.cpp *.h
-
-echo "*** cppcheck C program"
-cppcheck --enable=all --inconclusive --language=c --std=posix --suppress=missingIncludeSystem *.c
-
 echo "*** running"
 ./myprogram.exe
 
-echo "*** running with valgrind"
-valgrind ./myprogram.exe
+# echo "*** running with valgrind"
+# valgrind ./myprogram.exe
 
 echo "*** cleaning up"
 rm myprogram.exe
